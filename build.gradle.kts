792a18c36d8ad2d9e6aa1dd4908e0153947ac33c
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-21 15:39:54 +0200

    Add Gradle wrapper scripts and update build config
    Included Unix and Windows Gradle wrapper scripts for consistent build environment setup. Updated build configuration to use PaperMC repo and Lombok, and set Java toolchain to version 21.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,22 @@ group = "dev.luan.r6"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly ("org.projectlombok:lombok:1.18.34")
+    annotationProcessor ("org.projectlombok:lombok:1.18.34")
+
+    testCompileOnly ("org.projectlombok:lombok:1.18.34")
+    testAnnotationProcessor ("org.projectlombok:lombok:1.18.34")
+
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
 }
 
-tasks.test {
-    useJUnitPlatform()
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 }
